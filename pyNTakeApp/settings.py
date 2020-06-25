61213da4dbecd572b871eb20d91bb54904ce45b0
Zack Severino <60947431+zseverino@users.noreply.github.com>
2020-06-24 19:05:38 -0700

    Update settings.py

--- pyNTakeApp/settings.py
+++ pyNTakeApp/settings.py
@@ -142,8 +142,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 LOGIN_REDIRECT_URL = '/employeeIndex'
 
