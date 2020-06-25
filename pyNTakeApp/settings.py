c11e87fe95d82e6fd8f4db992131ad9709d63f1c
Zack Severino <60947431+zseverino@users.noreply.github.com>
2020-06-24 20:36:54 -0700

    Update settings.py

--- pyNTakeApp/settings.py
+++ pyNTakeApp/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
+import os, json
 import dj_database_url
 from google.oauth2 import service_account
 
