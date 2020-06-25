5d66c2958fdb01e07da2ab4e24d96260bab08ae1
Zack Severino <60947431+zseverino@users.noreply.github.com>
2020-06-25 11:22:03 -0700

    Update settings.py

--- pyNTakeApp/settings.py
+++ pyNTakeApp/settings.py
@@ -27,8 +27,8 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '.herokuapp.com']
 
