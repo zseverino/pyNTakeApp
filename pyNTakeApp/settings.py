a3c81d6eb0424bd036665d196498f979ccc77f3a
Zack Severino <60947431+zseverino@users.noreply.github.com>
2020-06-24 17:41:41 -0700

    Update settings.py

--- pyNTakeApp/settings.py
+++ pyNTakeApp/settings.py
@@ -34,6 +34,8 @@ DEFAULT_FILE_STORAGE = 'storages.backends.gcloud.GoogleCloudStorage'
 GS_BUCKET_NAME = 'pyntake-model-storage'
 GS_CREDENTIALS = os.environ['GS_CREDENTIALS']
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
